4b9d4f776c2ae19351b8abf16eb0ce67a4e38af0
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-03-05 18:50:01 +0100

    Initial commit

---
+++ src/main/java/com/example/java11jsr303customvalidation/Java11Jsr303CustomValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.java11jsr303customvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java11Jsr303CustomValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java11Jsr303CustomValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java11jsr303customvalidation/Java11Jsr303CustomValidationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.java11jsr303customvalidation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Java11Jsr303CustomValidationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
